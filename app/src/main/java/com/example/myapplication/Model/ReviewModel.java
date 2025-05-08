2a2c1252121a8b81a5fe2c53b2d86c30aa0daaf6
chinh <chinh31503@gmail.com>
2025-05-08 17:09:57 +0700

    Update Profile

---
+++ app/src/main/java/com/example/myapplication/Model/ReviewModel.java
@@ -0,0 +1,77 @@
+package com.example.myapplication.Model;
+
+public class ReviewModel {
+    private String content;
+    private String cafeName;
+    private int rating;
+    private int cafeId;
+    private long createdAt;
+    private int userId;
+    private String userName;
+    private String userAvatar;
+
+    public ReviewModel() {}
+    public String getCafeName() {
+        return cafeName;
+    }
+
+    public void setCafeName(String cafeName) {
+        this.cafeName = cafeName;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getCafeId() {
+        return cafeId;
+    }
+
+    public void setCafeId(int cafeId) {
+        this.cafeId = cafeId;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAvatar() {
+        return userAvatar;
+    }
+
+    public void setUserAvatar(String userAvatar) {
+        this.userAvatar = userAvatar;
+    }
+}
